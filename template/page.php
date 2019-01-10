e59cb4f0bfbfb02db04c6cb6432eacf64991f870
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-10 21:48:10 +0100

    addedInitPage

---
+++ template/page.php
@@ -0,0 +1,18 @@
+<?php return "<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <title>  $pageData->title</title>
+    $pageData->css
+</head>
+<body>
+$pageData->content 
+
+    
+</body>
+</html>
+"
+
+?>
