0704f580fff2026a661abd4ee1b0f84dc0b62789
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-10 21:47:44 +0100

    addedNavbar

---
+++ views/navigation.php
@@ -0,0 +1,8 @@
+<?php
+    return 
+    "<nav>
+        <li><a href='index.php?page=gallery'>Gallery</a></li>
+        <li><a href='index.php?page=upload'>Upload new iage</a></li>
+    </nav>
+    "
+?>
