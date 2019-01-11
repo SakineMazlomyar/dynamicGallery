506cbe24c8af864c008c2da264788f959ef0f7be
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-11 09:16:29 +0100

    addedUl

--- views/navigation.php
+++ views/navigation.php
@@ -1,8 +1,10 @@
 <?php
     return 
     "<nav>
-        <li><a href='index.php?page=gallery'>Gallery</a></li>
-        <li><a href='index.php?page=upload'>Upload new iage</a></li>
+        <ul>
+            <li><a href='index.php?page=gallery'>Gallery</a></li>
+            <li><a href='index.php?page=upload'>Upload new iage</a></li>
+        </ul>
     </nav>
     "
 ?>
